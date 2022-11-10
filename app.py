d95f7be96451b3e76a4b005f8e24d1429de47f35
mohammadsalmanmesam@gmail.com <mohammadsalmanmesam@gmail.com>
2022-11-10 14:58:27 -0500

    Adding Dockerization

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, request
-from calculator import Calculator as c
+from calculator import Calculator
 
 app = Flask(__name__)
 
@@ -12,21 +12,22 @@ def create_calc():
 
 @app.route('/', methods=['POST'])
 def calculate():
+    c = Calculator()
     v1 = request.form.get("v1", type=int)
     v2 = request.form.get("v2", type=int)
     operation = request.form.get("operation")
     if (operation == 'Add'):
         result = c.addition(v1, v2)
     elif (operation == 'Subtract'):
-        result = c.subtraction(2, 4)
+        result = c.subtraction(v1, v2)
     elif (operation == 'Multiply'):
-        result = c.multiplication(2, 4)
+        result = c.multiplication(v1, v2)
     elif (operation == 'Divide'):
-        result = c.division(2, 4)
+        result = c.division(v1, v2)
     else:
         result = 0
     return render_template('webcalc.html', computation=result)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))    
--- calculator.py
+++ calculator.py
@@ -1,30 +1,14 @@
 
 class Calculator:
 
-    def addition(n1, n2):
+    def addition(self, n1, n2):
         return n1+n2
 
-    def subtraction(n1, n2):
+    def subtraction(self, n1, n2):
         return n1-n2
 
-    def multiplication(n1, n2):
+    def multiplication(self, n1, n2):
         return n1*n2
 
-    def division(n1, n2):
+    def division(self, n1, n2):
         return n1/n2
-
-
-if __name__ == "__main__":
-    v1 = int(input("Enter your value 1: "))
-    v2 = int(input("Enter your value 2: "))
-    op = input("Enter your operator (a,s,m,d): ")
-    calc = Calculator
-
-    if (op == "a"):
-        print(calc.addition(v1, v2))
-    elif (op == "s"):
-        print(calc.subtraction(v1, v2))
-    elif (op == "m"):
-        print(calc.multiplication(v1, v2))
-    else:
-        print(calc.division(v1, v2))
