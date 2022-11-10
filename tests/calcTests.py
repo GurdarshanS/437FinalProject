7d021eb773ac6d7b4c3c03e4205f5ca469bd0b33
GurdarshanS <59708264+GurdarshanS@users.noreply.github.com>
2022-11-10 12:31:05 -0500

    Initial commit

---
+++ tests/calcTests.py
@@ -0,0 +1,18 @@
+import unittest
+from calcapp.calculator import Calculator as c
+
+class TestMethods(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(c.addition(2,4), 6)
+
+    def test_subtraction(self):
+        self.assertEqual(c.subtraction(2,4), -2)
+
+    def test_multiplication(self):
+        self.assertEqual(c.multiplication(2,4), 8)
+        
+    def test_division(self):
+        self.assertEqual(c.division(2,4), 0.5)
+        # a
+if __name__ == '__main__':
+    unittest.main()
