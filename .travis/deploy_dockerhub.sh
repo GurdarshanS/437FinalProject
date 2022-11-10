248fda2217e88f96e0c37730909ea48ae3774297
mohammadsalmanmesam@gmail.com <mohammadsalmanmesam@gmail.com>
2022-11-10 16:08:40 -0500

    try

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u 151214 -p salman1464
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t salman .
-docker push salman
+docker push 151214/salman
