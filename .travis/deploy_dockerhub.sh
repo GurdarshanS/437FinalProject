aaf1e44c840e98f983cd738305b0578a3a91e0ab
mohammadsalmanmesam@gmail.com <mohammadsalmanmesam@gmail.com>
2022-11-10 15:37:23 -0500

    fix

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t salman:$TAG .
-docker push 151214/salman:$TAG
+docker build -f Dockerfile -t salman .
+docker push 151214/salman
