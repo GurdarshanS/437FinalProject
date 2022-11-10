6b67eb435528a257e77fdb700b87ff197f7a22ea
mohammadsalmanmesam@gmail.com <mohammadsalmanmesam@gmail.com>
2022-11-10 15:10:41 -0500

    fixing tests

--- tests/calcTests.py
+++ tests/calcTests.py
@@ -1,18 +1,23 @@
 import unittest
-from calculator import Calculator as c
+from calculator import Calculator
 
 
 class TestMethods(unittest.TestCase):
+    
     def test_addition(self):
+        c = Calculator()
         self.assertEqual(c.addition(2, 4), 6)
 
     def test_subtraction(self):
+        c = Calculator()
         self.assertEqual(c.subtraction(2, 4), -2)
 
     def test_multiplication(self):
+        c = Calculator()
         self.assertEqual(c.multiplication(2, 4), 8)
 
     def test_division(self):
+        c = Calculator()
         self.assertEqual(c.division(2, 4), 0.5)
 
 
