3a00a6c7e91f60c6f8342764656a35c7ec2ba0b7
GurdarshanS <59708264+GurdarshanS@users.noreply.github.com>
2022-11-10 13:22:30 -0500

    Fixing Syntax Errors

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 
 class Calculator:
-    
+
     def addition(n1, n2):
         return n1+n2
 
@@ -19,12 +19,12 @@ if __name__ == "__main__":
     v2 = int(input("Enter your value 2: "))
     op = input("Enter your operator (a,s,m,d): ")
     calc = Calculator
-    
-    if(op=="a"):
-        print(calc.addition(v1,v2))
-    elif(op=="s"):
-        print(calc.subtraction(v1,v2))
-    elif(op=="m"):
-        print(calc.multiplication(v1,v2))
+
+    if (op == "a"):
+        print(calc.addition(v1, v2))
+    elif (op == "s"):
+        print(calc.subtraction(v1, v2))
+    elif (op == "m"):
+        print(calc.multiplication(v1, v2))
     else:
-        print(calc.division(v1,v2))
+        print(calc.division(v1, v2))
--- tests/calcTests.py
+++ tests/calcTests.py
@@ -1,18 +1,20 @@
 import unittest
 from calculator import Calculator as c
 
+
 class TestMethods(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(c.addition(2,4), 6)
+        self.assertEqual(c.addition(2, 4), 6)
 
     def test_subtraction(self):
-        self.assertEqual(c.subtraction(2,4), -2)
+        self.assertEqual(c.subtraction(2, 4), -2)
 
     def test_multiplication(self):
-        self.assertEqual(c.multiplication(2,4), 8)
-        
+        self.assertEqual(c.multiplication(2, 4), 8)
+
     def test_division(self):
-        self.assertEqual(c.division(2,4), 0.5)
+        self.assertEqual(c.division(2, 4), 0.5)
+
 
 if __name__ == '__main__':
     unittest.main()
