f822a07a3abfe51614facd57f27db9f4dec7eef5
mohammadsalmanmesam@gmail.com <mohammadsalmanmesam@gmail.com>
2022-11-10 18:00:54 -0500

    docker hub issue fix

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker login -u 151214 -p salman1464
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t majic .
-docker image push 151214/8dfoodh/majic:latest
+docker build -f Dockerfile -t $DOCKER_USER/8dfoodh:$TAG .
+docker push $DOCKER_USER/8dfoodh:$TAG
